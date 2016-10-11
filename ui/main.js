fbfeaa98be7890bd325ce1c9157b6a690ad5ea36
Andrew Rivett <veggiefrog@gmail.com>
2016-10-11 08:59:04 -0400

    Fixed bug with Hidden not being able to be unchecked

--- ui/main.js
+++ ui/main.js
@@ -2,110 +2,115 @@
 // The newsletters app 
 //
 function ciniki_newsletters_main() {
-    this.webFlags = {
-        '1':{'name':'Hidden'},
+    //
+    // Setup the main panel to list the collection
+    //
+    this.menu = new M.panel('Files',
+        'ciniki_newsletters_main', 'menu',
+        'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.menu');
+    this.menu.data = {};
+    this.menu.sections = {
+        'newsletters':{'label':'Newsletters',
+            'type':'simplegrid', 'num_cols':1,
+            'headerValues':null,
+            'cellClasses':[''],
+            'addTxt':'Add Newsletter',
+            'addFn':'M.ciniki_newsletters_main.showAdd(\'M.ciniki_newsletters_main.menu.open();\',\'1\');',
+            }
         };
-    this.fileTypes = {
-        '1':'Newsletter',
-        };
-    this.init = function() {
-        //
-        // Setup the main panel to list the collection
-        //
-        this.menu = new M.panel('Files',
-            'ciniki_newsletters_main', 'menu',
-            'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.menu');
-        this.menu.data = {};
-        this.menu.sections = {
-            'newsletters':{'label':'Newsletters',
-                'type':'simplegrid', 'num_cols':1,
-                'headerValues':null,
-                'cellClasses':[''],
-                'addTxt':'Add Newsletter',
-                'addFn':'M.ciniki_newsletters_main.showAdd(\'M.ciniki_newsletters_main.showMenu();\',\'1\');',
-                }
-            };
-        this.menu.cellValue = function(s, i, j, d) {
-            if( j == 0 ) { return d.file.name; }
-        };
-        this.menu.rowFn = function(s, i, d) {
-            return 'M.ciniki_newsletters_main.showEdit(\'M.ciniki_newsletters_main.showMenu();\', \'' + d.file.id + '\');'; 
-        };
-        this.menu.sectionData = function(s) { 
-            return this.data[s];
-        };
-        this.menu.addButton('add', 'Add', 'M.ciniki_newsletters_main.showAdd(\'M.ciniki_newsletters_main.showMenu();\');');
-        this.menu.addClose('Back');
+    this.menu.cellValue = function(s, i, j, d) {
+        if( j == 0 ) { return d.file.name; }
+    };
+    this.menu.rowFn = function(s, i, d) {
+        return 'M.ciniki_newsletters_main.showEdit(\'M.ciniki_newsletters_main.menu.open();\', \'' + d.file.id + '\');'; 
+    };
+    this.menu.sectionData = function(s) { 
+        return this.data[s];
+    };
+    this.menu.open = function(cb, listby, category) {
+        this.data = {};
+        M.api.getJSONCb('ciniki.newsletters.fileList', {'business_id':M.curBusinessID, 'type':'1'}, function(rsp) {
+            if( rsp.stat != 'ok' ) {
+                M.api.err(rsp);
+                return false;
+            }
+            var p = M.ciniki_newsletters_main.menu;
+            p.data = {'newsletters':rsp.files};
+            p.refresh();
+            p.show(cb);
+        });
+    };
+    this.menu.addButton('add', 'Add', 'M.ciniki_newsletters_main.showAdd(\'M.ciniki_newsletters_main.menu.open();\');');
+    this.menu.addClose('Back');
 
-        //
-        // The panel to display the add form
-        //
-        this.add = new M.panel('Add File',
-            'ciniki_newsletters_main', 'add',
-            'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.edit');
-        this.add.default_data = {'type':'1'};
-        this.add.data = {}; 
+    //
+    // The panel to display the add form
+    //
+    this.add = new M.panel('Add File',
+        'ciniki_newsletters_main', 'add',
+        'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.edit');
+    this.add.default_data = {'type':'1'};
+    this.add.data = {}; 
 // FIXME:       this.add.uploadDropFn = function() { return M.ciniki_newsletters_main.uploadDropImagesAdd; };
-        this.add.sections = {
-            '_file':{'label':'File', 'fields':{
-                'uploadfile':{'label':'', 'type':'file', 'hidelabel':'yes'},
-            }},
-            'info':{'label':'Information', 'type':'simpleform', 'fields':{
-                'name':{'label':'Title', 'type':'text'},
-                'publish_date':{'label':'Date', 'type':'date'},
-                'webflags':{'label':'Web', 'type':'flags', 'toggle':'yes', 'join':'yes', 'flags':this.webFlags},
-            }},
-            '_description':{'label':'Description', 'type':'simpleform', 'fields':{
-                'description':{'label':'', 'type':'textarea', 'size':'small', 'hidelabel':'yes'},
-            }},
-            '_save':{'label':'', 'buttons':{
-                'save':{'label':'Save', 'fn':'M.ciniki_newsletters_main.addFile();'},
-            }},
-        };
-        this.add.fieldValue = function(s, i, d) { 
-            if( this.data[i] != null ) {
-                return this.data[i]; 
-            } 
-            return ''; 
-        };
-        this.add.addButton('save', 'Save', 'M.ciniki_newsletters_main.addFile();');
-        this.add.addClose('Cancel');
-
-        //
-        // The panel to display the edit form
-        //
-        this.edit = new M.panel('File',
-            'ciniki_newsletters_main', 'edit',
-            'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.edit');
-        this.edit.file_id = 0;
-        this.edit.data = null;
-        this.edit.sections = {
-            'info':{'label':'Details', 'type':'simpleform', 'fields':{
-                'name':{'label':'Title', 'type':'text'},
-                'publish_date':{'label':'Date', 'type':'date'},
-                'webflags':{'label':'Web', 'type':'flags', 'toggle':'yes', 'join':'yes', 'flags':this.webFlags},
-            }},
-            '_description':{'label':'Description', 'type':'simpleform', 'fields':{
-                'description':{'label':'', 'type':'textarea', 'size':'small', 'hidelabel':'yes'},
-            }},
-            '_save':{'label':'', 'buttons':{
-                'save':{'label':'Save', 'fn':'M.ciniki_newsletters_main.saveFile();'},
-                'download':{'label':'Download', 'fn':'M.ciniki_newsletters_main.downloadFile(M.ciniki_newsletters_main.edit.file_id);'},
-                'delete':{'label':'Delete', 'fn':'M.ciniki_newsletters_main.deleteFile(M.ciniki_newsletters_main.edit.file_id);'},
-            }},
-        };
-        this.edit.fieldValue = function(s, i, d) { 
+    this.add.sections = {
+        '_file':{'label':'File', 'fields':{
+            'uploadfile':{'label':'', 'type':'file', 'hidelabel':'yes'},
+        }},
+        'info':{'label':'Information', 'type':'simpleform', 'fields':{
+            'name':{'label':'Title', 'type':'text'},
+            'publish_date':{'label':'Date', 'type':'date'},
+            'webflags':{'label':'Web', 'type':'flags', 'none':'yes', 'join':'yes', 'flags':{'1':{'name':'Hidden'}}},
+        }},
+        '_description':{'label':'Description', 'type':'simpleform', 'fields':{
+            'description':{'label':'', 'type':'textarea', 'size':'small', 'hidelabel':'yes'},
+        }},
+        '_save':{'label':'', 'buttons':{
+            'save':{'label':'Save', 'fn':'M.ciniki_newsletters_main.addFile();'},
+        }},
+    };
+    this.add.fieldValue = function(s, i, d) { 
+        if( this.data[i] != null ) {
             return this.data[i]; 
-        }
-        this.edit.sectionData = function(s) {
-            return this.data[s];
-        };
-        this.edit.fieldHistoryArgs = function(s, i) {
-            return {'method':'ciniki.newsletters.fileHistory', 'args':{'business_id':M.curBusinessID, 'file_id':this.file_id, 'field':i}};
-        };
-        this.edit.addButton('save', 'Save', 'M.ciniki_newsletters_main.saveFile();');
-        this.edit.addClose('Cancel');
+        } 
+        return ''; 
+    };
+    this.add.addButton('save', 'Save', 'M.ciniki_newsletters_main.addFile();');
+    this.add.addClose('Cancel');
+
+    //
+    // The panel to display the edit form
+    //
+    this.edit = new M.panel('File',
+        'ciniki_newsletters_main', 'edit',
+        'mc', 'medium', 'sectioned', 'ciniki.newsletters.main.edit');
+    this.edit.file_id = 0;
+    this.edit.data = null;
+    this.edit.sections = {
+        'info':{'label':'Details', 'type':'simpleform', 'fields':{
+            'name':{'label':'Title', 'type':'text'},
+            'publish_date':{'label':'Date', 'type':'date'},
+            'webflags':{'label':'Web', 'type':'flags', 'join':'yes', 'flags':{'1':{'name':'Hidden'}}},
+        }},
+        '_description':{'label':'Description', 'type':'simpleform', 'fields':{
+            'description':{'label':'', 'type':'textarea', 'size':'small', 'hidelabel':'yes'},
+        }},
+        '_save':{'label':'', 'buttons':{
+            'save':{'label':'Save', 'fn':'M.ciniki_newsletters_main.saveFile();'},
+            'download':{'label':'Download', 'fn':'M.ciniki_newsletters_main.downloadFile(M.ciniki_newsletters_main.edit.file_id);'},
+            'delete':{'label':'Delete', 'fn':'M.ciniki_newsletters_main.deleteFile(M.ciniki_newsletters_main.edit.file_id);'},
+        }},
+    };
+    this.edit.fieldValue = function(s, i, d) { 
+        return this.data[i]; 
     }
+    this.edit.sectionData = function(s) {
+        return this.data[s];
+    };
+    this.edit.fieldHistoryArgs = function(s, i) {
+        return {'method':'ciniki.newsletters.fileHistory', 'args':{'business_id':M.curBusinessID, 'file_id':this.file_id, 'field':i}};
+    };
+    this.edit.addButton('save', 'Save', 'M.ciniki_newsletters_main.saveFile();');
+    this.edit.addClose('Cancel');
 
     this.start = function(cb, appPrefix, aG) {
         args = {};
@@ -122,22 +127,8 @@ function ciniki_newsletters_main() {
             return false;
         }
 
-        this.showMenu(cb);
+        this.menu.open(cb);
     }
-
-    this.showMenu = function(cb, listby, category) {
-        this.menu.data = {};
-        var rsp = M.api.getJSONCb('ciniki.newsletters.fileList', {'business_id':M.curBusinessID, 'type':'1'}, function(rsp) {
-            if( rsp.stat != 'ok' ) {
-                M.api.err(rsp);
-                return false;
-            }
-            var p = M.ciniki_newsletters_main.menu;
-            p.data = {'newsletters':rsp.files};
-            p.refresh();
-            p.show(cb);
-        });
-    };
 
     this.showAdd = function(cb, type) {
         this.add.reset();
